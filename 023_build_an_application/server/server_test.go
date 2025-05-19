5e8726496230f42a1a144db5bc2d22f24c227dcf
Laurens van der Tas <laurens.vandertas@gmail.com>
2025-05-19 17:21:49 +0200

    update server_test.go

--- 023_build_an_application/server/server_test.go
+++ 023_build_an_application/server/server_test.go
@@ -13,8 +13,27 @@ func TestGETPlayers(t *testing.T) {
 	PlayerServer(response, request)
 
 	t.Run("returns Pepper's score", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodGet, "/players/Pepper", nil)
+		response := httptest.NewRecorder()
+
+		PlayerServer(response, request)
+
 		got := response.Body.String()
 		want := "20"
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+
+	t.Run("returns Floyd's score", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodGet, "/players/Floyd", nil)
+		response := httptest.NewRecorder()
+
+		PlayerServer(response, request)
+
+		got := response.Body.String()
+		want := "10"
 
 		if got != want {
 			t.Errorf("got %q, want %q", got, want)
