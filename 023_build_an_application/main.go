44e32470e7eeabf03efff6c537a55b15fccd3d5f
Laurens van der Tas <laurens.vandertas@gmail.com>
2025-05-19 17:43:42 +0200

    update main.go

--- 023_build_an_application/main.go
+++ 023_build_an_application/main.go
@@ -7,6 +7,12 @@ import (
 	"github.com/LPvdT/go-with-tests/application/server"
 )
 
+type InMemoryPlayerStore struct{}
+
+func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
+	return 123
+}
+
 func main() {
 	server := &server.PlayerServer{}
 	log.Fatal(http.ListenAndServe(":5000", server))
