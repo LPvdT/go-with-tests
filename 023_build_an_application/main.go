c246d2a92564f303231f9b83639c1d01bafc2e70
Laurens van der Tas <laurens.vandertas@gmail.com>
2025-05-24 15:26:05 +0200

    update main.go

--- 023_build_an_application/main.go
+++ 023_build_an_application/main.go
@@ -5,13 +5,23 @@ package main
 import (
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/LPvdT/go-with-tests/application/server"
 	"github.com/LPvdT/go-with-tests/application/store"
 )
 
+var (
+	port          string = "5000"
+	serverAddress string = strings.Join([]string{"localhost", port}, ":")
+)
+
 func main() {
 	store := store.NewInMemoryPlayerStore()
 	server := server.NewPlayerServer(store)
-	log.Fatal(http.ListenAndServe(":5000", server))
+
+	log.Printf("Starting server on http://%s", serverAddress)
+	if err := http.ListenAndServe(serverAddress, server); err != nil {
+		log.Fatalf("Could not start server: %v", err)
+	}
 }
