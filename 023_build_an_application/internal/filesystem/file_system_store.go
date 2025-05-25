6aa6a1200644c9f48bfb4dcc2adfa58725312967
Laurens van der Tas <laurens.vandertas@gmail.com>
2025-05-25 14:42:26 +0200

    update file_system_store.go

--- 023_build_an_application/internal/filesystem/file_system_store.go
+++ 023_build_an_application/internal/filesystem/file_system_store.go
@@ -1,6 +1,7 @@
 package filesystem
 
 import (
+	"encoding/json"
 	"io"
 	"log"
 
@@ -31,4 +32,24 @@ func (f *FileSystemPlayerStore) GetPlayerScore(name string) int {
 	}
 
 	return wins
+}
+
+func (f *FileSystemPlayerStore) RecordWin(name string) {
+	league := f.GetLeague()
+
+	for i, player := range league {
+		if player.Name == name {
+			league[i].Wins++
+		}
+	}
+
+	// Reset the reader to the start of the file
+	if _, err := f.database.Seek(0, io.SeekStart); err != nil {
+		log.Fatalf("could not seek to start of database: %v", err)
+	}
+
+	// Write the updated league back to the file
+	if err := json.NewEncoder(f.database).Encode(league); err != nil {
+		log.Fatalf("could not write to database: %v", err)
+	}
 }
