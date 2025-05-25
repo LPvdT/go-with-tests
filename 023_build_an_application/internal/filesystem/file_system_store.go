b3a8f782bb0220c1e4698c639c65620309603cb6
Laurens van der Tas <laurens.vandertas@gmail.com>
2025-05-25 14:23:38 +0200

    update file_system_store.go

--- 023_build_an_application/internal/filesystem/file_system_store.go
+++ 023_build_an_application/internal/filesystem/file_system_store.go
@@ -2,15 +2,20 @@ package filesystem
 
 import (
 	"io"
+	"log"
 
 	"github.com/LPvdT/go-with-tests/application/common"
 )
 
 type FileSystemPlayerStore struct {
-	database io.Reader
+	database io.ReadSeeker
 }
 
 func (f *FileSystemPlayerStore) GetLeague() []common.Player {
+	// Reset the reader to the start of the file
+	if _, err := f.database.Seek(0, io.SeekStart); err != nil {
+		log.Fatalf("could not seek to start of database: %v", err)
+	}
 	league, _ := common.NewLeague(f.database)
 	return league
 }
