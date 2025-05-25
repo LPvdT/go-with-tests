3c516c19dc2e7bbe036300bbedb318c1604949a4
Laurens van der Tas <laurens.vandertas@gmail.com>
2025-05-25 14:27:23 +0200

    update file_system_store_test.go

--- 023_build_an_application/internal/filesystem/file_system_store_test.go
+++ 023_build_an_application/internal/filesystem/file_system_store_test.go
@@ -25,4 +25,19 @@ func TestFileSystemStore(t *testing.T) {
 		common.AssertLeague(t, got_1, want)
 		common.AssertLeague(t, got_2, want)
 	})
+
+	t.Run("get player score", func(t *testing.T) {
+		database := strings.NewReader(`[
+			{"Name": "Cleo", "Wins": 10},
+			{"Name": "Chris", "Wins": 33}
+		]`)
+		store := FileSystemPlayerStore{database}
+
+		got := store.GetPlayerScore("Chris")
+		want := 33
+
+		if got != want {
+			t.Errorf("got %d want %d", got, want)
+		}
+	})
 }
