54e94bfeaca2bf44bb3f6a440b85850c3fde57a5
Laurens van der Tas <laurens.vandertas@gmail.com>
2025-05-26 02:59:18 +0200

    update file_system_store.go

--- 023_build_an_application/internal/filesystem/file_system_store.go
+++ 023_build_an_application/internal/filesystem/file_system_store.go
@@ -15,6 +15,10 @@ type FileSystemPlayerStore struct {
 	league   common.League
 }
 
+// NewFileSystemPlayerStore creates a new PlayerStore that stores data in a file.
+//
+// The file is expected to be a JSON file containing the current league.
+// The file is read when the store is created and written to when the store is updated.
 func NewFileSystemPlayerStore(file *os.File) *FileSystemPlayerStore {
 	_, err := file.Seek(0, io.SeekStart)
 	if err != nil {
@@ -29,10 +33,13 @@ func NewFileSystemPlayerStore(file *os.File) *FileSystemPlayerStore {
 	}
 }
 
+// GetLeague retrieves the current league state from the file.
 func (f *FileSystemPlayerStore) GetLeague() common.League {
 	return f.league
 }
 
+// GetPlayerScore returns the number of wins for a given player.
+// If the player is not found in the league, it returns 0.
 func (f *FileSystemPlayerStore) GetPlayerScore(name string) int {
 	player := f.league.Find(name)
 
@@ -43,6 +50,10 @@ func (f *FileSystemPlayerStore) GetPlayerScore(name string) int {
 	return 0
 }
 
+// RecordWin increments the win count for a player with the given name.
+//
+// If the player is not found, it adds a new player with a win count of 1.
+// After the win count is updated, it writes the updated league to the file.
 func (f *FileSystemPlayerStore) RecordWin(name string) {
 	player := f.league.Find(name)
 
