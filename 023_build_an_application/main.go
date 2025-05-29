3b24c59713c97a050c9023bc7a9cd9dae436562a
Laurens van der Tas <laurens.vandertas@gmail.com>
2025-05-29 13:00:56 +0200

    update main.go

--- 023_build_an_application/main.go
+++ 023_build_an_application/main.go
@@ -1,17 +1,15 @@
 // Continue here: https://quii.gitbook.io/learn-go-with-tests/build-an-application/io#didnt-we-just-break-some-rules-there-testing-private-things-no-interfaces
 
-// Repository: https://github.com/LPvdT/go-with-tests/tree/feature/json-routing-embedding
+// Repository: https://github.com/LPvdT/go-with-tests/tree/feature/io-sorting
 
 package main
 
 import (
-	"encoding/json"
 	"log"
 	"net/http"
 	"os"
 	"strings"
 
-	"github.com/LPvdT/go-with-tests/application/internal/common"
 	"github.com/LPvdT/go-with-tests/application/internal/filesystem"
 	"github.com/LPvdT/go-with-tests/application/server"
 )
@@ -32,8 +30,9 @@ func main() {
 	}
 
 	// Create a new file system player store with the database.
-	store := &filesystem.FileSystemPlayerStore{
-		Database: json.NewEncoder(&common.Tape{File: db}),
+	store, err := filesystem.NewFileSystemPlayerStore(db)
+	if err != nil {
+		log.Fatalf("problem creating file system player store: %v", err)
 	}
 
 	// Initialize the player server with the player store.
