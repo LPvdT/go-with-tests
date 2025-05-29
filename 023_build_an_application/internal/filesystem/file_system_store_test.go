cbbe0a7e077d097f163b9770dadc80bed498ac29
Laurens van der Tas <laurens.vandertas@gmail.com>
2025-05-29 13:25:21 +0200

    update file_system_store_test.go

--- 023_build_an_application/internal/filesystem/file_system_store_test.go
+++ 023_build_an_application/internal/filesystem/file_system_store_test.go
@@ -94,4 +94,24 @@ func TestFileSystemStore(t *testing.T) {
 
 		common.AssertNoError(t, err)
 	})
+
+	t.Run("league sorted", func(t *testing.T) {
+		database, cleanDatabase := common.CreateTempFile(t, `[
+		{"Name": "Cleo", "Wins": 10},
+		{"Name": "Chris", "Wins": 33}]`)
+		defer cleanDatabase()
+
+		store, err := NewFileSystemPlayerStore(database)
+		common.AssertNoError(t, err)
+
+		got := store.GetLeague()
+		want := common.League{
+			{Name: "Chris", Wins: 33},
+			{Name: "Cleo", Wins: 10},
+		}
+		common.AssertLeague(t, got, want)
+
+		got = store.GetLeague()
+		common.AssertLeague(t, got, want)
+	})
 }
