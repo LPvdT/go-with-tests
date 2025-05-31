1cb5fe885e1684251aee17956a58767598a02861
Laurens van der Tas <laurens.vandertas@gmail.com>
2025-05-31 11:21:56 +0200

    update main.go

--- 023_build_an_application/cmd/cli/main.go
+++ 023_build_an_application/cmd/cli/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/LPvdT/go-with-tests/application/internal/cli"
+	"github.com/LPvdT/go-with-tests/application/internal/filesystem"
+)
+
+const dbFileName = "game.db.json"
 
 func main() {
 	fmt.Println("Let's play poker!")
+	fmt.Println("Type {Name} wins to record a win for {Name}.")
+
+	db, err := os.OpenFile(dbFileName, os.O_RDWR|os.O_CREATE, 0o666)
+	if err != nil {
+		log.Fatalf("problem opening %s %v", dbFileName, err)
+	}
+
+	store, err := filesystem.NewFileSystemPlayerStore(db)
+	if err != nil {
+		log.Fatalf("problem creating file system player store, %v ", err)
+	}
+
+	game := cli.CLI{PlayerStore: store, In: os.Stdin}
+	game.PlayPoker()
 }
