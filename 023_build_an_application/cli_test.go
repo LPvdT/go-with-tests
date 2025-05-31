2c6619558dcfd37fa0d1bfe860fdb31eca638f59
Laurens van der Tas <laurens.vandertas@gmail.com>
2025-05-31 11:09:42 +0200

    update cli_test.go

--- 023_build_an_application/cli_test.go
+++ 023_build_an_application/cli_test.go
@@ -9,20 +9,11 @@ import (
 )
 
 func TestCLI(t *testing.T) {
-	in := strings.NewReader(("Cleo wins\n"))
+	in := strings.NewReader(("Chris wins\n"))
 	playerStore := &common.StubPlayerStore{}
 
 	cli := &cli.CLI{PlayerStore: playerStore, In: in}
 	cli.PlayPoker()
 
-	if len(playerStore.WinCalls) != 1 {
-		t.Fatal("expected a win call but didn't get any")
-	}
-
-	got := playerStore.WinCalls[0]
-	want := "Chris"
-
-	if got != want {
-		t.Errorf("didn't record correct winner, got %q, want %q", got, want)
-	}
+	common.AssertPlayerWin(t, playerStore, "Chris")
 }
