d28d325487857001df0e5f76af18602356acc772
Laurens van der Tas <laurens.vandertas@gmail.com>
2025-05-31 11:00:52 +0200

    update cli_test.go

--- 023_build_an_application/cli_test.go
+++ 023_build_an_application/cli_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/LPvdT/go-with-tests/application/internal/cli"
@@ -8,11 +9,20 @@ import (
 )
 
 func TestCLI(t *testing.T) {
+	in := strings.NewReader(("Cleo wins\n"))
 	playerStore := &common.StubPlayerStore{}
-	cli := &cli.CLI{PlayerStore: playerStore}
+
+	cli := &cli.CLI{PlayerStore: playerStore, In: in}
 	cli.PlayPoker()
 
 	if len(playerStore.WinCalls) != 1 {
-		t.Fatal("expected a call to Win() to be made, but it wasn't")
+		t.Fatal("expected a win call but didn't get any")
+	}
+
+	got := playerStore.WinCalls[0]
+	want := "Chris"
+
+	if got != want {
+		t.Errorf("didn't record correct winner, got %q, want %q", got, want)
 	}
 }
