d4ac3a0941dbb2aa2cb0a095e89b2754319456e9
Laurens van der Tas <laurens.vandertas@gmail.com>
2025-06-01 16:33:44 +0200

    update main.go

--- 023_build_an_application/cmd/cli/main.go
+++ 023_build_an_application/cmd/cli/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"log"
 	"os"
@@ -13,19 +12,13 @@ import (
 const dbFileName = "game.db.json"
 
 func main() {
-	fmt.Println("Let's play poker!")
-	fmt.Println("Type {Name} wins to record a win for {Name}.")
-
-	db, err := os.OpenFile(dbFileName, os.O_RDWR|os.O_CREATE, 0o666)
+	store, close, err := filesystem.FileSystemPlayerStoreFromFile(dbFileName)
 	if err != nil {
-		log.Fatalf("problem opening %s %v", dbFileName, err)
+		log.Fatal(err)
 	}
+	defer close()
 
-	store, err := filesystem.NewFileSystemPlayerStore(db)
-	if err != nil {
-		log.Fatalf("problem creating file system player store, %v ", err)
-	}
-
-	game := cli.CLI{PlayerStore: store, In: *bufio.NewScanner(os.Stdin)}
-	game.PlayPoker()
+	fmt.Println("Let's play poker")
+	fmt.Println("Type {Name} wins to record a win")
+	cli.NewCLI(store, os.Stdin).PlayPoker()
 }
