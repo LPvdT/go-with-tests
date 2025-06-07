8bcf3a62418707b748c2a61f6436dbe80f6fcd22
Laurens van der Tas <laurens.vandertas@gmail.com>
2025-06-07 14:50:13 +0200

    update cli.go

--- 023_build_an_application/internal/cli/cli.go
+++ 023_build_an_application/internal/cli/cli.go
@@ -41,17 +41,3 @@ func (cli *CLI) readLine() string {
 type BlindAlerter interface {
 	ScheduleAlertAt(duration time.Duration, amount int)
 }
-
-type SpyBlindAlerter struct {
-	Alerts []struct {
-		scheduledAt time.Duration
-		amount      int
-	}
-}
-
-func (s *SpyBlindAlerter) ScheduleAlertAt(duration time.Duration, amount int) {
-	s.Alerts = append(s.Alerts, struct {
-		scheduledAt time.Duration
-		amount      int
-	}{duration, amount})
-}
