e8cb725cf9873244af12ebe6f93cceeeac68cd05
Laurens van der Tas <laurens.vandertas@gmail.com>
2025-06-07 14:50:41 +0200

    update cli_test.go

--- 023_build_an_application/internal/cli/cli_test.go
+++ 023_build_an_application/internal/cli/cli_test.go
@@ -9,32 +9,10 @@ import (
 )
 
 func TestCLI(t *testing.T) {
-	t.Run("record chris win from user input", func(t *testing.T) {
-		in := strings.NewReader(("Chris wins\n"))
-		playerStore := &playertest.StubPlayerStore{}
-		blindAlerter := &cli.SpyBlindAlerter{}
-
-		cli := cli.NewCLI(playerStore, in, blindAlerter)
-		cli.PlayPoker()
-
-		playertest.AssertPlayerWin(t, playerStore, "Chris")
-	})
-
-	t.Run("record cleo win from user input", func(t *testing.T) {
-		in := strings.NewReader(("Cleo wins\n"))
-		playerStore := &playertest.StubPlayerStore{}
-		blindAlerter := &cli.SpyBlindAlerter{}
-
-		cli := cli.NewCLI(playerStore, in, blindAlerter)
-		cli.PlayPoker()
-
-		playertest.AssertPlayerWin(t, playerStore, "Cleo")
-	})
-
-	t.Run("it schedules printing of blind values", func(t *testing.T) {
+t.Run("it schedules printing of blind values", func(t *testing.T) {
 		in := strings.NewReader("Cleo wins\n")
 		playerStore := &playertest.StubPlayerStore{}
-		blindAlerter := &cli.SpyBlindAlerter{}
+		blindAlerter := &SpyBlindAlerter{}
 
 		cli := cli.NewCLI(playerStore, in, blindAlerter)
 		cli.PlayPoker()
@@ -43,4 +21,35 @@ func TestCLI(t *testing.T) {
 			t.Fatal("expected a blind alert to be scheduled")
 		}
 	})
+
+	t.Run("record chris win from user input", func(t *testing.T) {
+		in := strings.NewReader(("Chris wins\n"))
+		playerStore := &playertest.StubPlayerStore{}
+		
+		cli := cli.NewCLI(playerStore, in, dummySpyAlerter)
+		cli.PlayPoker()
+
+		playertest.AssertPlayerWin(t, playerStore, "Chris")
+	})
+
+	t.Run("record cleo win from user input", func(t *testing.T) {
+		in := strings.NewReader("Cleo wins\n")
+		playerStore := &playertest.StubPlayerStore{}
+		
+		cli := cli.NewCLI(playerStore, in, dummySpyAlerter)
+		cli.PlayPoker()
+
+		playertest.AssertPlayerWin(t, playerStore, "Cleo")
+	})
+
+// t.Run("do not read beyond the first newline", func(t *testing.T) {
+// 	in := failOnEndReader{
+	// 		t,
+	// 		strings.NewReader("Chris winds\n hello there"),
+	// 	}
+	// 	playerStore := &playertest.StubPlayerStore{}
+		
+	// 	cli := cli.NewCLI(playerStore, in, dummySpyAlerter)
+	// 	cli.PlayPoker()
+	// })
 }
