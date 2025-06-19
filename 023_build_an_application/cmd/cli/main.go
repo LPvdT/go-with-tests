6f57db1ae3ddd432f20481fe4d61b676a0d3ee25
Laurens van der Tas <laurens.vandertas@gmail.com>
2025-06-19 13:21:05 +0200

    update main.go

--- 023_build_an_application/cmd/cli/main.go
+++ 023_build_an_application/cmd/cli/main.go
@@ -1,17 +1,27 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
+	"log/slog"
 	"os"
 
 	"github.com/LPvdT/go-with-tests/application/internal/cli"
 	"github.com/LPvdT/go-with-tests/application/internal/filesystem"
+	_ "github.com/LPvdT/go-with-tests/application/internal/logging"
+	"github.com/lmittmann/tint"
 )
 
 const dbFileName = "game.db.json"
 
 func main() {
+	logger := slog.Default()
+
+	logger.Debug("Loading player store:", "filename", dbFileName)
+	logger.Info("Loading player store:", "filename", dbFileName)
+	logger.Warn("Loading player store:", "filename", dbFileName)
+	logger.Error("Loading player store:", tint.Err(errors.New("teun")), "filename", dbFileName)
 	store, close, err := filesystem.FileSystemPlayerStoreFromFile(dbFileName)
 	if err != nil {
 		log.Fatal(err)
