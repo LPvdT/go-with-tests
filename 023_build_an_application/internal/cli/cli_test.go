4f5d534d2fc543f69168cbf8cacc664aa95cceb7
Laurens van der Tas <laurens.vandertas@gmail.com>
2025-06-19 09:30:06 +0200

    update cli_test.go

--- 023_build_an_application/internal/cli/cli_test.go
+++ 023_build_an_application/internal/cli/cli_test.go
@@ -10,18 +10,21 @@ import (
 	"github.com/LPvdT/go-with-tests/application/playertest"
 )
 
-type SpyBlindAlerter struct {
-	Alerts []struct {
-		scheduledAt time.Duration
-		amount      int
-	}
+type ScheduledAlert struct {
+	scheduledAt time.Duration
+	amount      int
 }
 
-func (s *SpyBlindAlerter) ScheduleAlertAt(duration time.Duration, amount int) {
-	s.Alerts = append(s.Alerts, struct {
-		scheduledAt time.Duration
-		amount      int
-	}{duration, amount})
+func (s ScheduledAlert) String() string {
+	return fmt.Sprintf("%d chips at %v", s.amount, s.scheduledAt)
+}
+
+type SpyBlindAlerter struct {
+	alerts []ScheduledAlert
+}
+
+func (s *SpyBlindAlerter) ScheduleAlertAt(at time.Duration, amount int) {
+	s.alerts = append(s.alerts, ScheduledAlert{at, amount})
 }
 
 var dummySpyAlerter = &SpyBlindAlerter{}
