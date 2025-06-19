ce8e5ee43d83ec11f81e679cbdf2aa1cf40b3289
Laurens van der Tas <laurens.vandertas@gmail.com>
2025-06-19 07:52:59 +0200

    update cli_test.go

--- 023_build_an_application/internal/cli/cli_test.go
+++ 023_build_an_application/internal/cli/cli_test.go
@@ -1,6 +1,7 @@
 package cli_test
 
 import (
+	"fmt"
 	"strings"
 	"testing"
 	"time"
@@ -31,16 +32,49 @@ func (s *SpyBlindAlerter) ScheduleAlertAt(duration time.Duration, amount int) {
 var dummySpyAlerter = &SpyBlindAlerter{}
 
 func TestCLI(t *testing.T) {
-	t.Run("it schedules printing of blind values", func(t *testing.T) {
-		in := strings.NewReader("Cleo wins\n")
+	t.Run("it schedules the printing of blind values", func(t *testing.T) {
+		in := strings.NewReader("Chris wins\n")
 		playerStore := &playertest.StubPlayerStore{}
 		blindAlerter := &SpyBlindAlerter{}
 
 		cli := cli.NewCLI(playerStore, in, blindAlerter)
 		cli.PlayPoker()
 
-		if len(blindAlerter.Alerts) != 1 {
-			t.Fatal("expected a blind alert to be scheduled")
+		cases := []struct {
+			expectedScheduleTime time.Duration
+			expectedAmount       int
+		}{
+			{0 * time.Second, 100},
+			{10 * time.Minute, 200},
+			{20 * time.Minute, 300},
+			{30 * time.Minute, 400},
+			{40 * time.Minute, 500},
+			{50 * time.Minute, 600},
+			{60 * time.Minute, 800},
+			{70 * time.Minute, 1000},
+			{80 * time.Minute, 2000},
+			{90 * time.Minute, 4000},
+			{100 * time.Minute, 8000},
+		}
+
+		for i, c := range cases {
+			t.Run(fmt.Sprintf("%d scheduled for %v", c.expectedAmount, c.expectedScheduleTime), func(t *testing.T) {
+				if len(blindAlerter.Alerts) <= i {
+					t.Fatalf("alert %d was not scheduled %v", i, blindAlerter.Alerts)
+				}
+
+				alert := blindAlerter.Alerts[i]
+
+				amountGot := alert.amount
+				if amountGot != c.expectedAmount {
+					t.Errorf("got amount %d, want %d", amountGot, c.expectedAmount)
+				}
+
+				gotScheduledTime := alert.scheduledAt
+				if gotScheduledTime != c.expectedScheduleTime {
+					t.Errorf("got scheduled time %v, want %v", gotScheduledTime, c.expectedScheduleTime)
+				}
+			})
 		}
 	})
 
